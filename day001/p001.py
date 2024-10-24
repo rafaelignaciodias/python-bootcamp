1e66a22814003f8ea4205f3f635ed881380e0dca
Rafael Ignacio Dias <rafaelignaciodias@gmail.com>
2024-10-24 15:31:10 -0400

    day 001

---
+++ day001/p001.py
@@ -0,0 +1,12 @@
+# Band Name Generator
+# This program generates a band name based on user input.
+# It prompts the user for the name of the city they grew up in
+# and their pet's name, then combines them to create a unique band name.
+#
+# Author: iamraffa
+# Date: 24/10/2024
+
+print("Welcome to the Band Name Generator!")
+city_name = input("Hey! What city did you grow up in? \n")
+pet_name = input("What's your pet's name? \n")
+print("Your awesome band name could be " + city_name + " " + pet_name + "!")
